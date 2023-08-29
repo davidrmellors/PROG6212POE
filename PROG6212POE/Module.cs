568347ac880c4001d89b4214ab5cd846f5253c10
davidrmellors <davidroymellors@gmail.com>
2023-08-29 10:08:32 +0200

    first commit

---
+++ PROG6212POE/Module.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6212POE
+{
+    public class Module
+    {
+
+        public string ModuleCode { get; set; }
+        public string ModuleName { get; set; }
+        public int NumberOfCredits { get; set; }
+        public int ClassHoursPerWeek { get; set; }
+        public int NumberOfWeeks { get; set; }
+        public int StartDate { get; set; }
+
+
+
+    }
+}
